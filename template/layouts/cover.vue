98f0a8bdf768da99d9d1c4efe2e1c6e62888711e
Meena Murthy <meena.murthy@amarulasolutions.com>
2025-06-19 16:38:19 +0530

    Added tbody element
    Not having tbody for tables can lead to hydration errors or
    disrupt functionality in future
    
    Signed-off-by: Meena Murthy <meena.murthy@amarulasolutions.com>

--- template/layouts/cover.vue
+++ template/layouts/cover.vue
@@ -6,6 +6,7 @@
     </div>
   </div>
   <table class="bottompanel">
+  <tbody>
       <tr>
         <th style="text-align:left; padding-left:8px;">
           <a href="https://www.amarulasolutions.com">https://www.amarulasolutions.com</a>
@@ -14,5 +15,6 @@
           <SlideCurrentNo class="" />
         </th>
       </tr>
+    </tbody>
   </table>
 </template>
